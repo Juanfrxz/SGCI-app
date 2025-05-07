cd796a141070130d865180046384101de673df99
JoseDFN <63378897+JoseDFN@users.noreply.github.com>
2025-05-07 09:03:38 -0500

    feat: :sparkles: Add Dtos
    Added the Dto folder with the Dtos for client, employee and provider

---
+++ domain/DTO/DtoProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SGCI_app.domain.Entities;
+
+namespace SGCI_app.domain.DTO
+{
+    public class DtoProvider
+    {
+        public string? Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Email { get; set; }
+
+        // Se correlaciona mediante LinQ
+        public int TipoDoc_id { get; set; }
+        public int TipoTercero_id { get; set; }
+
+        public Address address { get; set; }
+        public Provider provider { get; set; }
+    }
+}
